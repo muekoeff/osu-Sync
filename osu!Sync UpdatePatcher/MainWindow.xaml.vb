cfa18fa6e675902a142f571ee1c91580e221e7f2
naseweis520 <kvmp2468@yahoo.de>
2015-05-02 17:28:46 +0200

    [Added] Updater: Display paths; [Added] Updater: Display unzipping-progress; [Changed] Updater: Using library "Ionic.Zip.dll" for ZIP-Files

--- osu!Sync UpdatePatcher/MainWindow.xaml.vb
+++ osu!Sync UpdatePatcher/MainWindow.xaml.vb
@@ -1,11 +1,12 @@
-﻿Imports System.ComponentModel, System.IO
+﻿Imports Ionic.Zip
+Imports System.ComponentModel, System.IO
 
 Class MainWindow
 
     Private Argument_deletePackageAfter As Boolean = True
     Private Argument_destinationVersion, Argument_sourceVersion, Argument_pathToApp, _
         Argument_pathToUpdate, Argument_updateHash As String
-    Private Updater_TempPath As String
+    Private Updater_ZipCount, Updater_ZipCurrentCount As Integer
     Private WithEvents Worker As New BackgroundWorker With { _
         .WorkerReportsProgress = True}
 
@@ -23,6 +24,14 @@ Class MainWindow
         Result = Strings.Replace(BitConverter.ToString(Hash), "-", "")
         Return Result
     End Function
+
+    Private Sub Action_ZipProgress(ByVal sender As Object, e As ExtractProgressEventArgs)
+        If Not e.TotalBytesToTransfer = 0 Then
+            Dim Percentage As Integer = CInt(e.BytesTransferred / e.TotalBytesToTransfer * 100)
+            Worker.ReportProgress(Nothing, "Unzipping... | " & Percentage & " %")
+            Worker.ReportProgress(Nothing, "[PROGRESSBAR] " & (Updater_ZipCount * 100) & ";" & (Updater_ZipCurrentCount * 100 + Percentage))
+        End If
+    End Sub
 
     Private Sub Button_closeUpdater_Click(sender As Object, e As RoutedEventArgs) Handles Button_closeUpdater.Click
         Application.Current.Shutdown()
@@ -59,17 +68,18 @@ Class MainWindow
 
     Private Sub Worker_DoWork(sender As Object, e As DoWorkEventArgs) Handles Worker.DoWork
         ' Check if osu!Sync is running
+        Worker.ReportProgress(Nothing, "Checking, if osu!Sync is running...")
         Dim MainProcess() As Process = Process.GetProcessesByName("osu!Sync")
         If MainProcess.Length > 0 Then
-            Worker.ReportProgress(Nothing, "Closing osu!Sync...")
+            Worker.ReportProgress(Nothing, "Killing osu!Sync...")
             MainProcess(0).Kill()
             MainProcess(0).WaitForExit()
         End If
 
         ' Assign variables
+        Worker.ReportProgress(Nothing, "Reading arguments...")
         For Each Current In I__StartUpArguments
             Dim CurrentSplit() As String = Current.Split(CChar("="))
-            CurrentSplit(1).Replace("\\", "\")      ' In order to correct a mistake from version 0.0.0.7
             Select Case CurrentSplit(0)
                 Case "-deletePackageAfter"
                     Argument_deletePackageAfter = CType(CurrentSplit(1), Boolean)
@@ -93,8 +103,10 @@ Class MainWindow
 
         ' Set VersionInfo
         Worker.ReportProgress(Nothing, "[VERSIONINFO] Update from " & Argument_sourceVersion & " to " & Argument_destinationVersion)
+        Worker.ReportProgress(Nothing, "[PATHS] " & Argument_pathToUpdate & ";" & Argument_pathToApp)
 
         ' Check if files exist
+        Worker.ReportProgress(Nothing, "Checking...")
         If Not IO.Directory.Exists(Argument_pathToApp) Then
             Worker.ReportProgress(Nothing, "Update failed!")
             MsgBox("The path to the application can't be found.", MsgBoxStyle.Exclamation, "Dialog | osu!Sync Software Update Patcher")
@@ -108,9 +120,10 @@ Class MainWindow
         End If
 
         ' Verify update package
-        Worker.ReportProgress(Nothing, "Verifying update package...")
+        Worker.ReportProgress(Nothing, "Verifying package...")
         If Not MD5FileHash(Argument_pathToUpdate) = Argument_updateHash Then
             Worker.ReportProgress(Nothing, "[NOT VERIFIED]")
+            Worker.ReportProgress(Nothing, "Waiting for user response")
             If MessageBox.Show("This update package seems to be different from the original update package online." & vbNewLine & "Do you still want to proceed?", "Dialog | osu!Sync Software Update Patcher", MessageBoxButton.YesNo, MessageBoxImage.Question) = MessageBoxResult.No Then
                 Worker.ReportProgress(Nothing, "[CANCEL]")
                 Exit Sub
@@ -120,6 +133,7 @@ Class MainWindow
         End If
 
         ' Ask to clear folder
+        Worker.ReportProgress(Nothing, "Waiting for user response")
         If MessageBox.Show("Do you want to clear the folder where osu!Sync is installed?" & vbNewLine & "This isn't necessary but will clean up no longer needed files (if there're some). Don't do that if there're other files except osu!Sync's files inside this folder." & vbNewLine & vbNewLine & "// Path: " & Argument_pathToApp, "Dialog | osu!Sync Software Update Patcher", MessageBoxButton.YesNo, MessageBoxImage.Question) = MessageBoxResult.Yes Then
             For Each SelectedFile In Directory.GetFiles(Argument_pathToApp)
                 Worker.ReportProgress(Nothing, "Clearing folder...")
@@ -127,13 +141,16 @@ Class MainWindow
             Next
         End If
 
-        ' Unzip update packag
-        Worker.ReportProgress(Nothing, "Unzipping update package...")
-        Dim sc As New Shell32.Shell()
-        Dim output As Shell32.Folder = sc.NameSpace(Argument_pathToApp)
-        Dim input As Shell32.Folder = sc.NameSpace(Argument_pathToUpdate)
-        Worker.ReportProgress(Nothing, "Moving files...")
-        output.CopyHere(input.Items, 4)
+        ' Unzip
+        Worker.ReportProgress(Nothing, "Unzipping...")
+        Using Zipper As ZipFile = ZipFile.Read(Argument_pathToUpdate)
+            AddHandler Zipper.ExtractProgress, AddressOf Action_ZipProgress
+            Updater_ZipCount = Zipper.Count
+            For Each ZipperEntry As ZipEntry In Zipper
+                ZipperEntry.Extract(Argument_pathToApp, ExtractExistingFileAction.OverwriteSilently)
+                Updater_ZipCurrentCount += 1
+            Next
+        End Using
 
         ' Delete update package
         If Argument_deletePackageAfter Then
@@ -141,29 +158,46 @@ Class MainWindow
             File.Delete(Argument_pathToUpdate)
         End If
 
-        Worker.ReportProgress(Nothing, "Update successfully finished!")
+        Worker.ReportProgress(Nothing, "[FINISHED]")
     End Sub
 
     Private Sub Worker_ProgressChanged(sender As Object, e As ProgressChangedEventArgs) Handles Worker.ProgressChanged
         If e.UserState.ToString().StartsWith("[VERSIONINFO] ") Then
             TextBlock_VersionInfo.Text = e.UserState.ToString().Substring("[VERSIONINFO] ".Length)
+        ElseIf e.UserState.ToString().StartsWith("[PATHS] ") Then
+            Dim Text() = e.UserState.ToString().Substring("[VERSIONINFO] ".Length).Split(CChar(";"))
+            With TextBlock_Paths
+                .Text = Text(1)
+                .ToolTip = "Path of Update Package: " & Text(0) & vbNewLine & _
+                    "Path to osu!Sync: " & Text(1)
+            End With
+        ElseIf e.UserState.ToString().StartsWith("[PROGRESSBAR] ") Then
+            Dim Text() = e.UserState.ToString().Substring("[VERSIONINFO] ".Length).Split(CChar(";"))
+            With ProgressBar
+                .IsIndeterminate = False
+                .Maximum = CDbl(Text(0))
+                .Value = CDbl(Text(1))
+            End With
         ElseIf e.UserState.ToString() = "[VERIFIED]" Then
             TextBlock_VersionInfo.Text += " | Update package verified"
         ElseIf e.UserState.ToString() = "[NOT VERIFIED]" Then
             TextBlock_VersionInfo.Text += " | Update package unverified"
         ElseIf e.UserState.ToString() = "[CANCEL]" Then
-            TextBlock_VersionInfo.Text = "Aborted!"
-            Application.Current.Shutdown()
-            Exit Sub
-        Else
-            TextBlock_CurrentProcess.Text = e.UserState.ToString()
-        End If
-
-        If e.UserState.ToString() = "Update successfully finished!" Then
+            Button_closeUpdater.Visibility = Windows.Visibility.Visible
+            With Button_startOsusync
+                .IsEnabled = False
+                .Visibility = Windows.Visibility.Visible
+            End With
+            ProgressBar.Visibility = Windows.Visibility.Hidden
+            TextBlock_CurrentProcess.Text = "Aborted!"
+        ElseIf e.UserState.ToString() = "[FINISHED]" Then
+            TextBlock_CurrentProcess.Text = "Update successfully finished!"
             Me.Cursor = Cursors.Arrow
             Button_closeUpdater.Visibility = Windows.Visibility.Visible
             Button_startOsusync.Visibility = Windows.Visibility.Visible
             ProgressBar.Visibility = Windows.Visibility.Hidden
+        Else
+            TextBlock_CurrentProcess.Text = e.UserState.ToString()
         End If
     End Sub
 End Class
