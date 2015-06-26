05d5b3e5bcc20b3bb17b01ff2240349301361b3a
naseweis520 <kvmp2468@yahoo.de>
2015-06-26 16:46:08 +0200

    [Changed] SplashScreen, [Dev] Set version to 1.0.0.0

--- osu!Sync/My Project/AssemblyInfo.vb
+++ osu!Sync/My Project/AssemblyInfo.vb
@@ -55,5 +55,5 @@ Imports System.Windows
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.0.13")> 
-<Assembly: AssemblyFileVersion("0.0.0.13")> 
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
