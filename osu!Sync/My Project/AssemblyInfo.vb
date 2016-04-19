1587cb9daebb58259939c553630f5f60ec3bd782
naseweis520 <kvmp2468@yahoo.de>
2016-04-19 20:04:18 +0200

    [Dev] Incremented version to 1.0.0.12

--- osu!Sync/My Project/AssemblyInfo.vb
+++ osu!Sync/My Project/AssemblyInfo.vb
@@ -55,5 +55,5 @@ Imports System.Windows
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.11")>
-<Assembly: AssemblyFileVersion("1.0.0.11")>
+<Assembly: AssemblyVersion("1.0.0.12")>
+<Assembly: AssemblyFileVersion("1.0.0.12")>
