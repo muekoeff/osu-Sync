2c7ceba54e2fa031a60703e6425da8cb76647e98
naseweis520 <me@nw520.de>
2017-01-09 17:39:54 +0100

    [Dev] Set version number to 1.0.1.0

---
+++ osu!Sync/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("osu!Sync")]
+[assembly: AssemblyDescription("Share your beatmaps.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("nw520 Technologies")]
+[assembly: AssemblyProduct("osu!Sync")]
+[assembly: AssemblyCopyright("2017 | Open-Source")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("da8900bf-73d7-45cb-a805-3ea353a3bb2d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
